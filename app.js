0641ee989a8928976c30ce9939a2f2ac41bb438a
Thuong Ho <thuong.t.ho@gmail.com>
2015-08-21 14:40:17 -0700

    add post method

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-  mongoose = require('mongoose');  // mongoose converts mongodb to json format
+  mongoose = require('mongoose'),  // mongoose converts mongodb to json format
+  bodyParser = require('body-parser');  // allows express to read the body and parse it into json
 
 // opens a connection to the db and if movieAPI doesn't exist, it will create it
 var db = mongoose.connect('mongodb://localhost/movieAPI');
@@ -11,10 +12,24 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+
+// Let the app know that we will use the body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); // look at body for json obj and add to req.body
+
 // Route handler
 var movieRouter = express.Router();
 
 movieRouter.route('/Movies')
+  .post(function(req, res) {
+    var movie = new Movie(req.body);  // pass in post data that's been sent to us
+
+    // console.log(movie);
+    movie.save();  // create new movie in MongoDB
+    // 201 status is created
+    res.status(201).send(movie);  // make id avail to client
+
+  })
   .get(function(req, res) {
     // var query = req.query;  // this method allows anything to query the db
     var query = {}; // more secure
