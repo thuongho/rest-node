341bea8dc108fb0650be82eacbc86413b4e4c0d0
Thuong Ho <thuong.t.ho@gmail.com>
2015-08-20 19:27:23 -0700

    add router

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+// Route handler
+var movieRouter = express.Router();
+
+movieRouter.route('/Movies')
+  .get(function(req, res) {
+    var responseJson = { greeting: "Movies galore!" };
+
+    res.json(responseJson);
+  });
+
+// path will be /api/Movies
+app.use('/api', movieRouter);
+
 // handler
 app.get('/', function(req, res) {
   res.send('welcome to my API!')
