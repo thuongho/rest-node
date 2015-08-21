07f13b244410396da600de00923ba0ef9227ce7f
Thuong Ho <thuong.t.ho@gmail.com>
2015-08-21 14:54:42 -0700

    organize routes

---
+++ Routes/movieRouter.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var routes = function() {
+
+  // Route handler
+  var movieRouter = express.Router();
+
+  movieRouter.route('/Movies')
+    .post(function(req, res) {
+      var movie = new Movie(req.body);  // pass in post data that's been sent to us
+
+      // console.log(movie);
+      movie.save();  // create new movie in MongoDB
+      // 201 status is created
+      res.status(201).send(movie);  // make id avail to client
+
+    })
+    .get(function(req, res) {
+      // var query = req.query;  // this method allows anything to query the db
+      var query = {}; // more secure
+
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      } 
+
+      // add query as a param to the find
+      Movie.find(query, function(err, movies) {
+        if (err) {
+          res.status(500).send(err); // display 500 error page with the error
+        } else {
+          res.json(movies);
+        }
+      });
+    });
+
+  movieRouter.route('/Movies/:movieId')
+    .get(function(req, res) {
+      Movie.findById(req.params.bookId, function(err, book) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(book);
+        }
+      });
+    });
+  // make the function return the movieRouter
+  return movieRouter;
+};
+
+module.exports = routes;
--- app.js
+++ app.js
@@ -17,47 +17,8 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // look at body for json obj and add to req.body
 
-// Route handler
-var movieRouter = express.Router();
-
-movieRouter.route('/Movies')
-  .post(function(req, res) {
-    var movie = new Movie(req.body);  // pass in post data that's been sent to us
-
-    // console.log(movie);
-    movie.save();  // create new movie in MongoDB
-    // 201 status is created
-    res.status(201).send(movie);  // make id avail to client
-
-  })
-  .get(function(req, res) {
-    // var query = req.query;  // this method allows anything to query the db
-    var query = {}; // more secure
-
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    } 
-
-    // add query as a param to the find
-    Movie.find(query, function(err, movies) {
-      if (err) {
-        res.status(500).send(err); // display 500 error page with the error
-      } else {
-        res.json(movies);
-      }
-    });
-  });
-
-movieRouter.route('/Movies/:movieId')
-  .get(function(req, res) {
-    Movie.findById(req.params.bookId, function(err, book) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.json(book);
-      }
-    });
-  });
+// execute the function to return back the movieRouter
+movieRouter = require('./Routes/movieRouter')();
 
 // path will be /api/Movies
 app.use('/api', movieRouter);
